8c0e07c3db9f2368b570c3e67941b386673bd763
HuangYuyang <huangyuyang1998@126.com>
2019-03-28 17:40:01 +0800

    i18n basic configuration
    home.html has been fully converted

--- src/main/java/com/anonymous/HST1C/config/HST1CWebInitializer.java
+++ src/main/java/com/anonymous/HST1C/config/HST1CWebInitializer.java
@@ -1,12 +1,17 @@
 package com.anonymous.HST1C.config;
 
 import com.anonymous.HST1C.web.WebConfig;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
 import javax.servlet.ServletRegistration;
 
 public class HST1CWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    private static final String TEMPORARY_DIRECTORY="/tmp/";
+
     @Override
     protected Class<?>[] getRootConfigClasses() {
         return new Class[]{RootConfig.class};
@@ -25,7 +30,14 @@ public class HST1CWebInitializer extends AbstractAnnotationConfigDispatcherServl
     @Override
     protected void customizeRegistration(ServletRegistration.Dynamic registration) {
         registration.setMultipartConfig(
-                new MultipartConfigElement("/tmp/")
+                new MultipartConfigElement(TEMPORARY_DIRECTORY)
         );
     }
+        @Override
+        protected Filter[] getServletFilters() {//<--Critical for database info decoding
+            CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+            characterEncodingFilter.setEncoding("UTF-8");
+            characterEncodingFilter.setForceEncoding(true);
+            return new Filter[] { characterEncodingFilter};
+        }
 }
--- src/main/java/com/anonymous/HST1C/web/WebConfig.java
+++ src/main/java/com/anonymous/HST1C/web/WebConfig.java
@@ -1,27 +1,40 @@
 package com.anonymous.HST1C.web;
 
-import java.io.IOException;
-
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
 import org.thymeleaf.spring3.SpringTemplateEngine;
 import org.thymeleaf.spring3.view.ThymeleafViewResolver;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.anonymous.HST1C.web")
-public class WebConfig  {
+public class WebConfig extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
         viewResolver.setTemplateEngine(templateEngine);
+        viewResolver.setCharacterEncoding("UTF-8");//<--Critical!
         return viewResolver;
     }
     @Bean
@@ -36,6 +49,7 @@ public class WebConfig  {
         TemplateResolver templateResolver = new ServletContextTemplateResolver();
         templateResolver.setPrefix("/WEB-INF/views/");
         templateResolver.setSuffix(".html");
+//        templateResolver.setCharacterEncoding("UTF-8");
         templateResolver.setTemplateMode("HTML5");
         return templateResolver;
     }
@@ -44,4 +58,47 @@ public class WebConfig  {
     public MultipartResolver multipartResolver() throws IOException{
         return new StandardServletMultipartResolver();
     }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+//                slr.setDefaultLocale(Locale.US);
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource =
+                new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");//<--Critical to enable resource properties
+//        messageSource.setUseCodeAsDefaultMessage(true);
+        messageSource.setCacheSeconds(10);
+        return messageSource;
+    }
+
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        converters.add(stringHttpMessageConverter());
+//    }
+//
+//    @Bean
+//     public StringHttpMessageConverter stringHttpMessageConverter() {
+//        StringHttpMessageConverter stringHttpMessageConverter= new StringHttpMessageConverter(
+//                Charset.forName("UTF-8")
+//        );
+//        stringHttpMessageConverter.setWriteAcceptCharset(false);
+//        return stringHttpMessageConverter;
+//    }
 }
