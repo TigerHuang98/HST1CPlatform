018581e87d8bd731be179c663e6e552cafb1666d
HuangYuyang <huangyuyang1998@126.com>
2019-05-15 19:53:46 +0800

    communicate icon bug fix staff side icon enhanced

--- src/main/java/com/anonymous/HST1C/web/FetchMessagesController.java
+++ src/main/java/com/anonymous/HST1C/web/FetchMessagesController.java
@@ -2,7 +2,10 @@ package com.anonymous.HST1C.web;
 
 import com.anonymous.HST1C.Message;
 import com.anonymous.HST1C.Role;
+import com.anonymous.HST1C.Userinfo;
 import com.anonymous.HST1C.data.MessageRepository;
+import com.anonymous.HST1C.data.OrderRepository;
+import com.anonymous.HST1C.data.UserinfoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +14,21 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.List;
 
 @Controller
 @RequestMapping("/fetch_message")
 public class FetchMessagesController {
     private MessageRepository messageRepository;
+    private OrderRepository orderRepository;
+    private UserinfoRepository userinfoRepository;
 
     @Autowired
-    public FetchMessagesController(MessageRepository messageRepository) {
+    public FetchMessagesController(MessageRepository messageRepository, OrderRepository orderRepository, UserinfoRepository userinfoRepository) {
         this.messageRepository = messageRepository;
+        this.orderRepository = orderRepository;
+        this.userinfoRepository = userinfoRepository;
     }
 
     @RequestMapping(method = RequestMethod.POST)
@@ -41,6 +49,20 @@ public class FetchMessagesController {
             }
             messages.addAll(messageRepository.findMessagesByOrdernumber(hiddenMessageForm.getOrdernumber()));
             if(hiddenMessageForm.getRole()== Role.STAFF) {
+                if(messages.size()!=0){
+                    Userinfo userinfo=userinfoRepository.findUserinfo(
+                            orderRepository.findOrderByOrdernumber(
+                                    messageRepository.findMessageById(
+                                            messages.get(0).getMessageid()
+                                    ).getOrdernumber()
+                            ).getUsername()
+                    );
+                    if(userinfo!=null){
+                        byte[] userIconBytes = userinfo.getIconBytes();
+                        String userIconBase64 = Base64.getEncoder().encodeToString(userIconBytes);
+                        model.addFlashAttribute("user_pic",userIconBase64);
+                    }
+                }
                 for (Message message : messages) {
                     message.setIssend(!message.isSend());
                 }
